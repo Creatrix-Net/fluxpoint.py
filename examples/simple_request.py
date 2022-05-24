44bb754937e4bec6ee4c2287d878780f208488fb
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-05-24 15:46:33 +0530

    update examples accordingt o deepsourve

--- examples/simple_request.py
+++ examples/simple_request.py
@@ -7,7 +7,7 @@ a = FluxpointClient(
     api_token="get api token from https://fluxpoint.dev/api/access")
 
 # setting up the windows loop policy according to the operating system
-if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+if sys.platform.startswith(('win32', 'cygwin')):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 # getting the image url of AZURLANE image
