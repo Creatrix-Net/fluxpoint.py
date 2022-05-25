987c55a2384716ee7177d3460e4bcd84114960d1
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-05-25 15:56:52 +0530

    completed the misc portion of the api

--- fluxpoint/paths/misc.py
+++ fluxpoint/paths/misc.py
@@ -7,14 +7,22 @@ class AboutUs:
     :param json_data: The json data from the api
     :type json_data: dict
 
-    Acessible Parameter
-    --------------------
-
-    - id str
+    :Acessible Paramters:
+        :id: [:class:`int`] ID of the person 
+        :name: [:class:`str`] Discord username of the person 
+        :token: [:class:`str`] Fluxpoint token of the person 
+        :created: [:class:`datetime.datetime`] Account creation date
+        :expire: [Optional[:class:`datetime.datetime`]] Expiration date
+        :donator: [:class:`bool`] If the perso is a dontor in fluxpoint or not
+        :owner: [:class:`bool`] If the person is owner or not
+        :disabled: [:class:`bool`] If the acc is disabled or not
+        :status: [:class:`str`] HTTP Status
+        :code: [:class:`int`] HTTP Status code
+        :message: [:class:`str`] Message returned by the api
     """
     def __init__(self, json_data: dict) -> None:     
         for i in json_data:
-            setattr(self, i, json_data[i])
+            setattr(self, i.lower(), json_data[i])
 
 class Misc(BaseHTTP):
     """NSFW Api endpoints documented in https://bluedocs.page/fluxpoint-api/misc"""
