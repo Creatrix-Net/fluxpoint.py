c637db8189733e231c2e20109ede34061ed43d02
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-05-31 08:35:42 +0530

    added minecraft ping endpoint to

--- fluxpoint/paths/misc.py
+++ fluxpoint/paths/misc.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from ..enums import RequestTypes
 from ..http import BaseHTTP
 
@@ -25,6 +27,41 @@ class AboutUs:
     def __init__(self, json_data: dict) -> None:
         for i in json_data:
             setattr(self, i.lower(), json_data[i])
+    
+    def __str__(self) -> str:
+        return f"<AboutUs {self.name} ({self.id})>"
+
+
+class MinecraftPingData:
+    """A class which gives info from the json data of Minecraft server ping
+
+    :param json_data: The json data from the api
+    :type json_data: dict
+
+    :Acessible Paramters:
+        :online: [:class:`bool`] If the server is online or not
+        :icon: [:class:`str`] URL of the server icon
+        "motd": [:class:`str`] serverinfo
+        :playersOnline: [:class:`int`] Amount of players online
+        :playersMax: [:class:`int`] Max amount of players
+        :version: [:class:`str`] Minecraft version
+        :fullQuery: [:class:`bool`] If the query was full or not
+        :players: [Optional[:class:`list`]] List of players
+        :status: [:class:`str`] HTTP Status
+        :code: [:class:`int`] HTTP Status code
+        :message: [:class:`str`] Message returned by the api
+    """
+
+    def __init__(self, json_data: dict) -> None:
+        for i in json_data:
+            setattr(self, i.lower(), json_data[i])
+    
+    def __str__(self) -> str:
+        return '<Minecraft Server Ping Data>'
+    
+    def __repr__(self) -> str:
+        return '<Minecraft Server Ping Data>'
+
 
 
 class Misc(BaseHTTP):
@@ -40,3 +77,13 @@ class Misc(BaseHTTP):
         :rtype: AboutUs
         """
         return AboutUs(await self.request(RequestTypes.GET, 'me'))
+    
+    async def mc_ping(self, host: str, port: Optional[int] = 25565) -> MinecraftPingData:
+        """Ping a minecraft server
+
+        :param host: The hostname of the server
+        :type host: str
+        :param port: The port of the server
+        :type port: int
+        """
+        return MinecraftPingData(await self.request(RequestTypes.GET, f'mc/ping?host={host}&{port}'))
